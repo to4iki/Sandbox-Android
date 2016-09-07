38bcd9e4de6f93f9cdce10a863945df1ec8755d9
tsk takezawa <tsk.take815@gmail.com>
2016-09-07 12:40:17 +0900

    Create FastlaneBox app

---
+++ FastlaneBox/app/src/main/java/com/github/to4iki/fastlanebox/MainActivity.java
@@ -0,0 +1,13 @@
+package com.github.to4iki.fastlanebox;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
